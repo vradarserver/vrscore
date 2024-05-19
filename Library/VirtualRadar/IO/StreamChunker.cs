566c98b598c91eaf1ca61102a1aae615d04a45df
Andrew Whewell <andrew@whewell.co.uk>
2024-05-19 04:14:47 +0100

    WIP StreamChunker

---
+++ Library/VirtualRadar/IO/StreamChunker.cs
@@ -0,0 +1,190 @@
+﻿// Copyright © 2024 onwards, Andrew Whewell
+// All rights reserved.
+//
+// Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System.Buffers;
+using VirtualRadar.Buffers;
+
+namespace VirtualRadar.IO
+{
+    /// <summary>
+    /// A base class for objects that read bytes from a stream and separate them into chunks.
+    /// </summary>
+    /// <remarks><para>
+    /// Chunks are discrete messages that we are reading from a stream. They have an identifiable
+    /// start and end, which might require some inside-knowledge about the data that we expect to
+    /// see on the stream, but generally speaking chunkers do not care about the content of the
+    /// chunk. Something else will decode the chunk once the chunker has broken it out of the
+    /// stream.
+    /// </para>
+    /// </remarks>
+    public abstract class StreamChunker
+    {
+        /// <summary>
+        /// Used to allocate byte arrays that are exposed to the wider world.
+        /// </summary>
+        public static IsolatedMemoryPool _IsolatedPool = new();
+
+        /// <summary>
+        /// The maximum length of a chunk. If a sequence of bytes has not been identified
+        /// as a chunk before this limit is reached then the chunk is abandoned and the
+        /// chunker begins looking for the start of the next chunk.
+        /// </summary>
+        public virtual int MaximumChunkSize { get; } = 100;
+
+        /// <summary>
+        /// The number of chunks seen by the chunker over its current run of <see cref="ReadChunksFromStream"/>.
+        /// This is reset every time the function is called.
+        /// </summary>
+        public long CountChunksExtracted { get; private set; }
+
+        /// <summary>
+        /// Raised when a chunk is extracted. The chunk is passed as a block of memory. IT
+        /// WILL BE RELEASED OR REUSED AS SOON AS THE EVENT HANDLER RETURNS. If you need to
+        /// use the chunk after the event handler returns then you must copy it and use the
+        /// copy.
+        /// </summary>
+        public event EventHandler<ReadOnlyMemory<byte>> ChunkRead;
+
+        /// <summary>
+        /// Raises <see cref="ChunkRead"/>.
+        /// </summary>
+        /// <param name="args"></param>
+        protected virtual void OnChunkQueued(ReadOnlyMemory<byte> args)
+        {
+            ChunkRead?.Invoke(this, args);
+        }
+
+        /// <summary>
+        /// Reads chunks from the stream forever until the cancellation token is set or it errors
+        /// out. Each chunk is exposed via the <see cref="ChunkRead"/> event.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task ReadChunksFromStream(Stream stream, CancellationToken cancellationToken)
+        {
+            using(var readBuffer = MemoryPool<byte>.Shared.Rent(10)) {
+                while(!cancellationToken.IsCancellationRequested) {
+                    var usableLength = await stream.ReadAsync(readBuffer.Memory, cancellationToken);
+                    if(usableLength > 0) {
+                        ExtractChunksFromReadBuffer(
+                            readBuffer.Memory.Span[..usableLength]
+                        );
+                    }
+                }
+            }
+        }
+
+        private void ExtractChunksFromReadBuffer(Span<byte> readBuffer)
+        {
+            var parseable = readBuffer;
+
+            do {
+                var startOutcome = FindStartOfChunk(parseable);
+                if(startOutcome.StartOffset == -1) {
+                    parseable = [];
+                } else {
+                    var endOutcome = FindEndOfChunk(parseable[startOutcome.StartOffset..]);
+
+                    var chunkLength = endOutcome.EndOffset + 1;
+                    using(var chunk = _IsolatedPool.Rent(chunkLength)) {
+                        parseable
+                            .Slice(startOutcome.StartOffset, chunkLength)
+                            .CopyTo(chunk.Memory.Span);
+                        OnChunkQueued(chunk.Memory[..chunkLength]);
+                    }
+
+                    if(chunkLength <= parseable.Length) {
+                        parseable = parseable[(startOutcome.StartOffset + chunkLength)..];
+                    }
+                }
+            } while(parseable.Length > 0);
+        }
+
+        /// <summary>
+        /// Called at the start of <see cref="ReadChunksFromStream"/> to give derivees the
+        /// opportunity to reset state for the start of a fresh stream. This is called after
+        /// the base has reset its own state. The base does not need to be called.
+        /// </summary>
+        protected virtual void ResetStateForNewStream()
+        {
+            // Do not put code here, it would rely on the derivee calling it.
+        }
+
+        /// <summary>
+        /// Returns a tuple indicating either the start offset of the chunk in the buffer or the number
+        /// of bytes at the start of the buffer that are known to be garbage.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        private (int StartOffset, int DiscardLength) FindStartOfChunk(Span<byte> buffer)
+        {
+            var startOffset = -1;
+            var discardLength = 0;
+
+            var window = buffer;
+
+            for(var offset = 0;offset < buffer.Length;++offset) {
+                startOffset = FindStartOffsetInWindow(window);
+                if(startOffset == -1) {
+                    ++discardLength;
+                } else {
+                    break;
+                }
+                window = window[1..];
+            }
+
+            return (startOffset, discardLength);
+        }
+
+        /// <summary>
+        /// Returns the offset of the start of the chunk within the window passed across.
+        /// </summary>
+        /// <param name="window"></param>
+        /// <returns></returns>
+        protected abstract int FindStartOffsetInWindow(Span<byte> window);
+
+        /// <summary>
+        /// Returns a tuple indicating the offset of the last byte of the chunk or an indication that
+        /// the chunk has exceeded the maximum length without the end being seen, and so should be
+        /// discarded and a new search started.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        private (int EndOffset, int DiscardLength) FindEndOfChunk(Span<byte> buffer)
+        {
+            var endOffset = -1;
+            var discardLength = 0;
+
+            var window = buffer;
+
+            for(var offset = 0;offset < buffer.Length;++offset) {
+                endOffset = FindEndOffsetInWindow(window);
+                if(endOffset != -1) {
+                    break;
+                }
+                window = window[1..];
+            }
+
+            if(endOffset == -1 && buffer.Length >= MaximumChunkSize) {
+                discardLength = MaximumChunkSize;
+            }
+
+            return (endOffset, discardLength);
+        }
+
+        /// <summary>
+        /// Returns the offset of the end of the chunk within the window passed across.
+        /// </summary>
+        /// <param name="window"></param>
+        /// <returns></returns>
+        protected abstract int FindEndOffsetInWindow(Span<byte> window);
+    }
+}
---
+++ Tests/Tests.VirtualRadar/IO/StreamChunker_Tests.cs
@@ -0,0 +1,180 @@
+﻿// Copyright © 2024 onwards, Andrew Whewell
+// All rights reserved.
+//
+// Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//    * Neither the name of the author nor the names of the program's contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using Tests.Mocks;
+using VirtualRadar.IO;
+
+namespace Tests.VirtualRadar.IO
+{
+    [TestClass]
+    public class StreamChunker_Tests
+    {
+        class TestChunker : StreamChunker
+        {
+            private readonly byte[][] _StartMarkers;
+
+            private readonly byte[][] _EndMarkers;
+
+            private readonly bool _ReadAhead;
+
+            public override int MaximumChunkSize { get; }
+
+            public TestChunker(byte[][] startMarkers, byte[][] endMarkers, int maximumChunkSize, bool readAhead)
+            {
+                _StartMarkers = startMarkers;
+                _EndMarkers = endMarkers;
+                _ReadAhead = readAhead;
+                MaximumChunkSize = maximumChunkSize;
+            }
+
+            protected override int FindEndOffsetInWindow(Span<byte> window) => FindMarkerInWindow(_EndMarkers, window);
+
+            protected override int FindStartOffsetInWindow(Span<byte> window) => FindMarkerInWindow(_StartMarkers, window);
+
+            private int FindMarkerInWindow(byte[][] markers, Span<byte> window)
+            {
+                var result = -1;
+
+                for(var markerIdx = 0;markerIdx < markers.Length;++markerIdx) {
+                    var marker = markers[markerIdx];
+                    if(marker.Length == 0) {
+                        result = 0;
+                    } else {
+                        var matchedToIdx = 0;
+                        for(var windowIdx = 0;windowIdx < window.Length;++windowIdx) {
+                            matchedToIdx = window[windowIdx] == marker[matchedToIdx]
+                                ? matchedToIdx + 1
+                                : 0;
+                            if(matchedToIdx == 0 && !_ReadAhead) {
+                                break;
+                            }
+                            if(matchedToIdx == marker.Length) {
+                                result = windowIdx - (matchedToIdx - 1);
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        private TestChunker                         _TestChunker;
+        private Action<ReadOnlyMemory<byte>>        _ChunkExtractedCallback;
+        private int                                 _CountChunksSeen;
+        private CancellationTokenSource             _CancellationTokenSource;
+        private CancellationToken                   _CancellationToken;
+        private MockReadOnlyStream                  _Stream;
+
+        [TestInitialize]
+        public void TestInitialise()
+        {
+            _CancellationTokenSource = new();
+            _CancellationToken = _CancellationTokenSource.Token;
+            _Stream = new();
+            _Stream.StreamFinished += (_,_) => _CancellationTokenSource.Cancel();
+
+            _CountChunksSeen = 0;
+            _ChunkExtractedCallback = null;
+            _TestChunker = CreateTestChunker();
+        }
+
+        private TestChunker CreateTestChunker(
+            byte[][] startMarkers = null,
+            byte[][] endMarkers = null,
+            int maxChunkSize = 10,
+            bool readAhead = true,
+            bool setFields = true,
+            Action<ReadOnlyMemory<byte>> callback = null
+        )
+        {
+            var result = new TestChunker(
+                startMarkers ?? [ [ 0x00, ], ],
+                endMarkers   ?? [ [ 0xff, ], ],
+                maximumChunkSize:   maxChunkSize,
+                readAhead:          readAhead
+            );
+
+            callback ??= (args) => _ChunkExtractedCallback?.Invoke(args);
+            result.ChunkRead += (_,chunk) => {
+                ++_CountChunksSeen;
+                callback(chunk);
+            };
+
+            if(setFields) {
+                _TestChunker = result;
+            }
+
+            return result;
+        }
+
+        private void AssertChunk(ReadOnlySpan<byte> expected, ReadOnlyMemory<byte> actual, string message = null)
+        {
+            var areEqual = expected.SequenceEqual(actual.Span);
+            if(!areEqual && message == null) {
+                message = $"Expected to see [{String.Join(", ", expected.ToArray().Select(r => r.ToString("X2")))}] " +
+                          $"actual was [{String.Join(", ", actual.ToArray().Select(r => r.ToString("X2")))}]";
+            }
+            Assert.IsTrue(areEqual, message);
+        }
+
+        [TestMethod]
+        public async Task Read_Exposes_Chunk_From_Stream()
+        {
+            _Stream.Configure([ 0x00, 0xff, ], sendOnePacket: true);
+            _ChunkExtractedCallback = chunk => AssertChunk([ 0x00, 0xff, ], chunk);
+
+            await _TestChunker.ReadChunksFromStream(_Stream, _CancellationToken);
+
+            Assert.AreEqual(1, _CountChunksSeen);
+        }
+
+        [TestMethod]
+        public async Task Read_Discards_Bytes_Before_Start_Marker()
+        {
+            _Stream.Configure([ 0x01, 0x00, 0xff, ], sendOnePacket: true);
+            _ChunkExtractedCallback = chunk => AssertChunk([ 0x00, 0xff, ], chunk);
+
+            await _TestChunker.ReadChunksFromStream(_Stream, _CancellationToken);
+
+            Assert.AreEqual(1, _CountChunksSeen);
+        }
+
+        [TestMethod]
+        public async Task Read_Reads_Up_To_End_Marker()
+        {
+            _Stream.Configure([ 0x01, 0x00, 0xff, 0x01 ], sendOnePacket: true);
+            _ChunkExtractedCallback = chunk => AssertChunk([ 0x00, 0xff, ], chunk);
+
+            await _TestChunker.ReadChunksFromStream(_Stream, _CancellationToken);
+
+            Assert.AreEqual(1, _CountChunksSeen);
+        }
+
+        [TestMethod]
+        public async Task Read_Can_Read_Two_Chunks_From_One_Packet()
+        {
+            _Stream.Configure([
+                0x00, 0x01, 0xff,
+                0x00, 0x02, 0xff,
+            ], sendOnePacket: true);
+            _ChunkExtractedCallback = chunk => AssertChunk(_CountChunksSeen == 1
+                ? [ 0x00, 0x01, 0xff ]
+                : [ 0x00, 0x02, 0xff ],
+                chunk
+            );
+
+            await _TestChunker.ReadChunksFromStream(_Stream, _CancellationToken);
+
+            Assert.AreEqual(2, _CountChunksSeen);
+        }
+    }
+}
