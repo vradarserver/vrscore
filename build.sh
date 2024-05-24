ab0e4433d79b62cf72f47720545dac1f5e3deffc
Andrew Whewell <andrew@whewell.co.uk>
2024-05-24 01:27:16 +0100

    Add bash script versions of build and run

---
+++ build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+SHDIR="$(cd "$(dirname "$0")" && pwd)"
+
+SHOW_USAGE() {
+    echo "Usage: build.sh command options"
+    echo "restore      Restore all NuGet packages"
+    echo "solution     Build the solution"
+    echo "console      Build the console"
+    echo "terminal     Build the terminal"
+    echo
+    echo "-debug       Use Debug configuration (default)"
+    echo "-nobuild     Skip the build phase"
+    echo "-release     Use Release configuration"
+    echo "-run         Run the target after compilation"
+}
+
+BUILD_DOTNET() {
+    if [ "$BUILD" = "YES" ]; then
+        echo
+        echo dotnet build --configuration $CONFIG "$1" /p:"SolutionDir=$SHDIR"
+             dotnet build --configuration $CONFIG "$1" /p:"SolutionDir=$SHDIR"
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+    fi
+}
+
+RUN_PROGRAM() {
+    if [ "$RUN" = "YES" ]; then
+        $1 ${RUNARGS[@]}
+        exit $?
+    fi
+}
+
+RUN_DOTNET() {
+    RUNARGS=("$1" "${RUNARGS[@]}")
+    RUN_PROGRAM "dotnet"
+}
+
+TARGET=""
+CONFIG=Debug
+BUILD=YES
+RUN=NO
+declare -a RUNARGS=()
+
+for arg in "$@"
+do
+    if [ "$RUN" = "YES" ]; then
+        RUNARGS+=("$arg")
+    else
+        case $arg in
+            solution | console | terminal | restore)
+                TARGET="$arg"
+                ;;
+            -run)
+                RUN=YES
+                ;;
+            -debug)
+                CONFIG=Debug
+                ;;
+            -release)
+                CONFIG=Release
+                ;;
+            -nobuild)
+                BUILD=NO
+                ;;
+            *)
+                SHOW_USAGE
+                exit 1
+                ;;
+        esac
+    fi
+done
+
+case $TARGET in
+    console)
+        BUILD_DOTNET "$SHDIR/Utility/Console/Console.csproj"
+        RUN_DOTNET "$SHDIR/Utility/Console/bin/$CONFIG/net8.0/Console.dll"
+        ;;
+    restore)
+        dotnet restore "$SHDIR/VirtualRadar.sln"
+        ;;
+    solution)
+        BUILD_DOTNET "$SHDIR/VirtualRadar.sln"
+        RUNARGS="The run option makes no sense for the solution, ignoring it"
+        RUN_PROGRAM "echo"
+        ;;
+    terminal)
+        BUILD_DOTNET "$SHDIR/Utility/Terminal/Terminal.csproj"
+        RUN_DOTNET "$SHDIR/Utility/Terminal/bin/$CONFIG/net8.0/Terminal.dll"
+        ;;
+    *)
+        SHOW_USAGE
+        exit 1
+        ;;
+esac
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+SHDIR="$(cd "$(dirname "$0")" && pwd)"
+RUNARGS=${@:2}
+
+SHOW_USAGE() {
+    echo "Usage: run [program] (args to program)"
+    echo "console        Command console"
+    echo "terminal       Terminal"
+}
+
+RUN_BUILD() {
+    "$SHDIR/build.sh" "$1" -nobuild -run ${RUNARGS[@]}
+}
+
+if [ "$#" -eq 0 ]; then
+    SHOW_USAGE
+    exit 1
+fi
+
+case $1 in
+    console)
+        RUN_BUILD console
+        ;;
+    terminal)
+        RUN_BUILD terminal
+        ;;
+esac
