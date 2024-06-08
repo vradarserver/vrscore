afc31d46eab3631386f9cb4b08a0131ade44fc20
Andrew Whewell <vradarserver@users.noreply.github.com>
2024-06-08 23:06:50 +0100

    Add basic Blazor server-side render interactive app with test web API endpoint

---
+++ Apps/VirtualRadar.Server/ApiControllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualRadar.Server.ApiControllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("api/test/input")]
+        public IActionResult RepeatInput(string input)
+        {
+            return Ok(input);
+        }
+    }
+}
---
+++ Apps/VirtualRadar.Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using VirtualRadar.Server.Components;
+
+namespace VirtualRadar.Server
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddMvc(options => {
+                options.EnableEndpointRouting = false;
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+            builder.Logging.ClearProviders();
+
+            var app = builder.Build();
+
+            if(!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseMvcWithDefaultRoute();
+            app.UseAntiforgery();
+            app.MapRazorComponents<App>()
+               .AddInteractiveServerRenderMode();
+
+            Console.WriteLine($"Starting server");
+            var cancellationSource = new CancellationTokenSource();
+            var task = app.StartAsync(cancellationSource.Token);
+
+            Console.TreatControlCAsInput = true;
+            Console.WriteLine("Press Q to quit");
+
+            while(!Console.KeyAvailable) {
+                if(cancellationSource.IsCancellationRequested) {
+                    break;
+                }
+                Thread.Sleep(1);
+
+                while(Console.KeyAvailable) {
+                    if(Console.ReadKey(intercept: true).Key == ConsoleKey.Q) {
+                        cancellationSource.Cancel();
+                        break;
+                    }
+                }
+            }
+
+            await task;
+
+            return 0;
+        }
+    }
+}
