6d2e65b6cfbf7388d474ec3ed6419533b3681d04
Andrew Whewell <vradarserver@users.noreply.github.com>
2024-06-21 00:10:15 +0100

    Remove TestResults in clean.bat

--- CleanObject.ps1
+++ CleanObject.ps1
@@ -8,3 +8,6 @@ Dir -Path "$SourceRoot" bin -Directory -Recurse | Remove-Item -Force -Recurse
 
 Write-Output "Removing Distributables folder from $SourceRoot\DevOps"
 Dir -Path "$SourceRoot\DevOps" Distributables -Directory -Recurse | Remove-Item -Force -Recurse
+
+Write-Output "Removing TestResults folder from $SourceRoot"
+Dir -Path "$SourceRoot" TestResults -Directory | Remove-Item -Force -Recurse
