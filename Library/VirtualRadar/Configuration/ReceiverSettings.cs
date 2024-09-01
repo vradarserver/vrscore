e69c5412e63c0f7bef8f10255de5758b7135e05d
Andrew Whewell <vradarserver@users.noreply.github.com>
2024-09-01 16:39:01 +0100

    Make Receiversettings a record

--- Library/VirtualRadar/Configuration/ReceiverSettings.cs
+++ Library/VirtualRadar/Configuration/ReceiverSettings.cs
@@ -3,14 +3,31 @@
     /// <summary>
     /// Describes the configuration of a receiver.
     /// </summary>
-    public class ReceiverSettings
+    public record ReceiverSettings
     {
-        public string Name { get; set; }
+        public string Name { get; init; }
 
-        public ISettingsProvider Connector { get; set; }
+        public bool Enabled { get; init; }
 
-        public ISettingsProvider Translator { get; set; }
+        public ISettingsProvider Connector { get; init; }
 
-        public ISettingsProvider AircraftList { get; set; }
+        public ISettingsProvider Translator { get; init; }
+
+        public ISettingsProvider AircraftList { get; init; }
+
+        public ReceiverSettings(
+            string name = null,
+            bool enabled = true,
+            ISettingsProvider connector = null,
+            ISettingsProvider translator = null,
+            ISettingsProvider aircraftList = null
+        )
+        {
+            Name = name ?? "Receiver";
+            Enabled = enabled;
+            Connector = connector;
+            Translator = translator;
+            AircraftList = aircraftList;
+        }
     }
 }
