f99a6985a2206a8f4a73c4ee99082cbd39d91ece
Andrew Whewell <vradarserver@users.noreply.github.com>
2025-03-16 23:26:59 +0000

    Return a clone of the image bytes

--- Library/VirtualRadar.Drawing.MagickDotNet/Image.cs
+++ Library/VirtualRadar.Drawing.MagickDotNet/Image.cs
@@ -8,6 +8,7 @@
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OF THE SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+using System.IO;
 using ImageMagick;
 
 namespace VirtualRadar.Drawing.MagickDotNet
@@ -72,7 +73,10 @@ namespace VirtualRadar.Drawing.MagickDotNet
         {
             var magickFormat = imageFormat.ToMagickFormat();
             lock(_SyncLock) {
-                return _Image.ToByteArray(magickFormat);
+                var imageBytes = _Image.ToByteArray(magickFormat);
+                var result = new byte[imageBytes.Length];
+                Array.Copy(imageBytes, result, result.Length);
+                return result;
             }
         }
 
