4c5501e6650242bb845cfd9eb01870c4996f149b
Andrew Whewell <vradarserver@users.noreply.github.com>
2025-12-10 18:36:12 +0000

    WIP port ImageSharp graphics

--- Library/VirtualRadar.Drawing.ImageSharp/ImageWrapper.cs
+++ Library/VirtualRadar.Drawing.ImageSharp/ImageWrapper.cs
@@ -321,5 +321,37 @@ namespace VirtualRadar.Drawing.ImageSharp
             var clone = _Native.Clone(context => context.Rotate(degrees));
             return new(clone, isCachedOriginal: false);
         }
+
+        public ImageWrapper AddTextLines(IEnumerable<string> textLines, bool centreText, bool isHighDpi)
+        {
+            var result = _Native.Clone(context => {
+                    var lines =          textLines.Where(tl => tl != null).ToList();
+                    var lineHeight =     isHighDpi ? 24f : 12f;
+                    var topOffset =      5f;
+                    var startPointSize = isHighDpi ? 20f : 10f;
+                    var outlinePen =     isHighDpi ? _MarkerTextOutlinePenHiDpi : _MarkerTextOutlinePen;
+                    var left =           centreText ? ((float)Width / 2.0F) : outlinePen.StrokeWidth / 2.0F;
+                    var top =            (Height - topOffset) - (lines.Count * lineHeight);
+                    var width =          Math.Max(0F, Width - outlinePen.StrokeWidth);
+
+                    var lineTop = top;
+                    foreach(var line in lines) {
+                        using(var fontAndText = _FontFactory.GetFontForRectangle(drawing, _MarkerTextFontFamily, _MarkerTextFontStyle, startPointSize, 6.0F, width, lineHeight * 2F, line, useCache: true)) {
+                            drawing.DrawText(
+                                fontAndText.Text,
+                                fontAndText.Font,
+                                _MarkerTextFillBrush,
+                                _MarkerTextOutlinePen,
+                                left,
+                                lineTop,
+                                centreText ? VrsDrawing.HorizontalAlignment.Centre : VrsDrawing.HorizontalAlignment.Left,
+                                preferSpeedOverQuality: false
+                            );
+                        }
+
+                        lineTop += lineHeight;
+                    }
+            });
+        }
     }
 }
